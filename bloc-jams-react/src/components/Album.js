ab1ef0df1afc5bfe17f676c560f72d46c6d50932
Jill <globaldetroithuman@gmail.com>
2018-06-21 17:58:51 -0400

    CP7 Complete

--- bloc-jams-react/src/components/Album.js
+++ bloc-jams-react/src/components/Album.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
-import { Link } from 'react-router-dom';
 
 
 class Album extends Component {
@@ -11,9 +10,36 @@ class Album extends Component {
 		});
 
 		this.state = {
-			album: album
+			album: album,
+      		currentSong: album.songs[0],
+      		isPlaying: false
 		};
+
+		this.audioElement = document.createElement('audio');
+     	this.audioElement.src = album.songs[0].audioSrc;
 	}
+
+	play() {
+     this.audioElement.play();
+     this.setState({ isPlaying: true });
+   	}
+   	pause() {
+   		this.audioElement.pause();
+   		this.setState({ isPlaying: false });
+   	}
+   	setSong(song) {
+   		this.audioElement.src = song.audioSrc;
+   		this.setState({ currentSong: song });
+   	}	
+   	handleSongClick(song) {
+   		const isSameSong = this.state.currentSong === song;
+   		if (this.state.isPlaying && isSameSong) {
+   			this.pause();
+   		} else {
+   			if (!isSameSong) { this.setSong(song); }
+   			this.play();
+   		}
+   	}
 
 
 
@@ -37,14 +63,14 @@ class Album extends Component {
 					<tbody>
 					{
 					this.state.album.songs.map( (song, index) => 
-							<div key = { index } >
-								<div>
+								<tr className="song" key={index} onClick={() => this.handleSongClick(song)} >	 
+									<div>
 									<tr id="song-index"> {index + 1} </tr>
 									<tr id="song-title">{song.title}</tr>
 									<tr id="song-duration">{song.duration}</tr>
 									<tr id="song-audioSrc">{song.audioSrc}</tr>
-								</div>
-							</div>	
+							</div>
+							</tr>
 					)						
 					}		
 					</tbody>
